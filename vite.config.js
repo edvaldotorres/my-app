495e473101d1b92ef1e1ea081bef89e3ca1269aa
edvaldotorres <edvaldo.torres@tisaude.com>
2023-04-27 10:59:41 -0300

    :wrench: docker config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        host: true,
+        stringPort: "8080",
+        port: 8080,
+    }
 })
